b3e0b44917b779188c087f6fca9b358eb563f366
kaancitak <72071892+kaancitak@users.noreply.github.com>
2020-12-14 12:28:20 +0100

    update 8

--- eigenaars.php
+++ eigenaars.php
@@ -177,3 +177,40 @@ VALUES ('{$_GET['naam']}', '{$_GET['telefoonnummer']}', '{$_GET['email']}', '{$_
     }
 }
 ?>
+<!doctype html>
+<html lang="nl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>Eigenaar</title>
+  </head>
+  <body>
+    <form method="GET">
+        <input type="hidden" name="actie" value="">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>EIGENAAR</h1>
+                </div>
+            </div>
+            <hr>
+            <?php print kiesDier1($arrDier,$idCurrentDier); ?>
+            <?php print formDier1($arrDier,$idCurrentDier); ?>
+            <?php print buttonBar1($idCurrentDier) ?>
+        </div>
+    </form>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
+
